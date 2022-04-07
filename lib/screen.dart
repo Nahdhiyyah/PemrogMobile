6cd1ac82bb98fad57224863eb66f14bb6a83baca
FlutLab.io <>
2022-04-07 14:55:25 +0000

    Autosave

--- lib/screen.dart
+++ lib/screen.dart
@@ -74,13 +74,12 @@ class AboutPage extends StatelessWidget {
         title: Text('Tentang Aplikasi'),
       ),
       body: Container(
-          padding: EdgeInsets.all(30),
-          child: Center(
-            child: Text(
-              "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
-              style: TextStyle(color: Colors.black, fontWeight: FontWeight.bold, fontSize: 17),
-            ),
-          )),
+        padding: EdgeInsets.all(30),
+        child: Text(
+          "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+          style: TextStyle(color: Colors.black, fontWeight: FontWeight.bold, fontSize: 17),
+        ),
+      ),
     );
   }
 }
